2acfefa0da712239e9edcca9855d324a05932c8c
chsami <sami.chkhachkhi@gmail.com>
2025-09-09 13:03:57 +0200

    fix: build

--- src/main/java/net/runelite/client/plugins/microbot/npcTanner/npcTannerPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/npcTanner/npcTannerPlugin.java
@@ -7,7 +7,6 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.plugins.microbot.LunarTablets.LunarTabletsPlugin;
 import net.runelite.client.plugins.microbot.PluginConstants;
 import net.runelite.client.plugins.microbot.util.antiban.Rs2Antiban;
 import net.runelite.client.plugins.microbot.util.antiban.enums.Activity;
