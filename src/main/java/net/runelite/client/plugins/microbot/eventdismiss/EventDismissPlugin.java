8e78e22263af8d1cad8064f597e1e41887676cae
chsami <sami.chkhachkhi@gmail.com>
2025-09-14 00:46:42 +0200

    feat: eventdismiss

---
+++ src/main/java/net/runelite/client/plugins/microbot/eventdismiss/EventDismissPlugin.java
@@ -0,0 +1,54 @@
+package net.runelite.client.plugins.microbot.eventdismiss;
+
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.PluginConstants;
+import net.runelite.client.plugins.microbot.driftnet.DriftNetPlugin;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+import static net.runelite.client.plugins.PluginDescriptor.Mocrosoft;
+
+@PluginDescriptor(
+        name = PluginDescriptor.Default + "Event Dismiss</html>",
+        description = "Random Event Dismisser",
+        tags = {"random", "events", "microbot"},
+        authors = {"Unknown"},
+        version = EventDismissPlugin.version,
+        minClientVersion = "2.0.7",
+        cardUrl = "https://chsami.github.io/Microbot-Hub/EventDismissPlugin/assets/card.jpg",
+        iconUrl = "https://chsami.github.io/Microbot-Hub/EventDismissPlugin/assets/icon.jpg",
+        enabledByDefault = PluginConstants.DEFAULT_ENABLED,
+        isExternal = PluginConstants.IS_EXTERNAL
+)
+@Slf4j
+public class EventDismissPlugin extends Plugin {
+    public static final String version = "1.0.0";
+    @Inject
+    private ConfigManager configManager;
+    @Inject
+    private EventDismissConfig config;
+
+    private DismissNpcEvent dismissNpcEvent;
+
+    @Provides
+    EventDismissConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(EventDismissConfig.class);
+    }
+
+    @Override
+    protected void startUp() throws AWTException {
+        dismissNpcEvent = new DismissNpcEvent(config);
+        Microbot.getBlockingEventManager().add(dismissNpcEvent);
+    }
+
+    protected void shutDown() {
+        Microbot.getBlockingEventManager().remove(dismissNpcEvent);
+        dismissNpcEvent = null;
+    }
+}
