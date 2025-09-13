ddf2bee66efacfed2f760fc7d6c1b80a656e0eab
chsami <sami.chkhachkhi@gmail.com>
2025-09-13 19:02:25 +0200

    feat: natehumidifier plugin

---
+++ src/main/java/net/runelite/client/plugins/microbot/natehumidifier/HumidifierOverlay.java
@@ -0,0 +1,53 @@
+package net.runelite.client.plugins.microbot.natehumidifier;
+
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierPlugin;
+import net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierScript;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+
+
+public class HumidifierOverlay extends OverlayPanel {
+
+    @Inject
+    HumidifierOverlay(HumidifierPlugin plugin)
+    {
+        super(plugin);
+        setPosition(OverlayPosition.TOP_LEFT);
+        setNaughty();
+    }
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        try {
+
+            panelComponent.setPreferredSize(new Dimension(275, 800));
+            panelComponent.getChildren().add(TitleComponent.builder()
+                    .text("Nate's Humidifier V" + net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierScript.version)
+                    .color(Color.magenta)
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left(net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierScript.itemsProcessedMessage)
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left(HumidifierScript.profitMessage)
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left(Microbot.status)
+                    .build());
+
+
+        } catch(Exception ex) {
+            Microbot.logStackTrace(this.getClass().getSimpleName(), ex);
+        }
+        return super.render(graphics);
+    }
+}
---
+++ src/main/java/net/runelite/client/plugins/microbot/natehumidifier/HumidifierPlugin.java
@@ -0,0 +1,59 @@
+package net.runelite.client.plugins.microbot.natehumidifier;
+
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierConfig;
+import net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierOverlay;
+import net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierScript;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+
+@PluginDescriptor(
+        name = PluginDescriptor.Nate +"Humidifier",
+        description = "Nate's Humidifier",
+        tags = {"magic", "nate", "humidifier","moneymaking"},
+        enabledByDefault = false
+)
+@Slf4j
+public class HumidifierPlugin extends Plugin {
+    @Inject
+    private net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierConfig config;
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private HumidifierOverlay humidifierOverlay;
+
+    @Inject
+    HumidifierScript humidifierScript;
+
+    @Provides
+    net.runelite.client.plugins.microbot.nateplugins.moneymaking.natehumidifier.HumidifierConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(HumidifierConfig.class);
+    }
+
+
+    @Override
+    protected void startUp() throws AWTException {
+		Microbot.pauseAllScripts.compareAndSet(true, false);
+        if (overlayManager != null) {
+            overlayManager.add(humidifierOverlay);
+        }
+
+        if (Microbot.getClient() == null)
+            shutDown();
+
+        humidifierScript.run(config);
+    }
+
+    protected void shutDown() {
+        humidifierScript.shutdown();
+        overlayManager.remove(humidifierOverlay);
+    }
+}
