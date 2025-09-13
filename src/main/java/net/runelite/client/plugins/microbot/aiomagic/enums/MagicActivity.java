1745d846f85e913d50eca96e780f37aa8ee3c88b
chsami <sami.chkhachkhi@gmail.com>
2025-09-13 21:23:50 +0200

    fix: aiomagic imports

---
+++ src/main/java/net/runelite/client/plugins/microbot/aiomagic/enums/MagicActivity.java
@@ -0,0 +1,12 @@
+package net.runelite.client.plugins.microbot.aiomagic.enums;
+
+public enum MagicActivity {
+    SPLASHING,
+    ALCHING,
+    SUPERHEAT,
+    TELEPORT,
+    TELEALCH,
+    STUNALCH,
+    STUNTELEALCH, // NEW
+    SPINFLAX,
+}
