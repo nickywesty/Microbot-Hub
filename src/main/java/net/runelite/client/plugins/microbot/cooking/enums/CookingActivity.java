d16664f908393149e9759ee5caa37ee4ee6d9183
chsami <sami.chkhachkhi@gmail.com>
2025-09-13 22:49:15 +0200

    feat: cooking

---
+++ src/main/java/net/runelite/client/plugins/microbot/cooking/enums/CookingActivity.java
@@ -0,0 +1,12 @@
+package net.runelite.client.plugins.microbot.cooking.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum CookingActivity {
+    COOKING,
+    BURN_BAKING
+//    REFILLING,
+//    WINE,
+//    BAKING,
+}
