85e4e1d2cc3d65b701bdcc6ac1e4c229b6596f2f
chsami <sami.chkhachkhi@gmail.com>
2025-09-13 21:25:48 +0200

    fix: aiofighter and animatedarmour imports

--- src/main/java/net/runelite/client/plugins/microbot/aiofighter/AIOFighterPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/aiofighter/AIOFighterPlugin.java
@@ -52,17 +52,17 @@ import java.util.stream.Collectors;
         name = PluginDescriptor.Mocrosoft + "AIO Fighter",
         authors = { "Mocrosoft", "See1Duck" },
         version = AIOFighterPlugin.version,
-        minClientVersion = "1.9.6",
+        minClientVersion = "2.0.7",
         description = "Microbot AIO Fighter plugin",
         tags = {"fight", "microbot", "misc", "combat", "playerassistant"},
-        cardUrl = "https://chsami.github.io/Microbot-Hub/AnonymousPlugin/assets/card.png",
-        iconUrl = "https://chsami.github.io/Microbot-Hub/AnonymousPlugin/assets/icon.png",
+        cardUrl = "https://chsami.github.io/Microbot-Hub/AIOFighterPlugin/assets/card.png",
+        iconUrl = "https://chsami.github.io/Microbot-Hub/AIOFighterPlugin/assets/icon.png",
         enabledByDefault = PluginConstants.DEFAULT_ENABLED,
         isExternal = PluginConstants.IS_EXTERNAL
 )
 @Slf4j
 public class AIOFighterPlugin extends Plugin {
-    public static final String version = "2.0.3 BETA";
+    public static final String version = "2.0.4";
     public static boolean needShopping = false;
     private static final String SET = "Set";
     private static final String CENTER_TILE = ColorUtil.wrapWithColorTag("Center Tile", JagexColors.MENU_TARGET);
