1cf6fea5e43a9181d805e66f2d1bcd86f3dab244
chsami <sami.chkhachkhi@gmail.com>
2025-09-14 02:26:31 +0200

    fix: roguesden import

--- src/main/java/net/runelite/client/plugins/microbot/roguesden/RoguesDenPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/roguesden/RoguesDenPlugin.java
@@ -10,7 +10,6 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.microbot.PluginConstants;
-import net.runelite.client.plugins.microbot.birdhunter.BirdHunterPlugin;
 import net.runelite.client.ui.overlay.OverlayManager;
 
 import javax.inject.Inject;
