dcf3318970b9966bcf3f01ae268bf0ba403b775e
davidja92 <david.04dc5@gmail.com>
2025-09-29 20:31:18 -0700

    added nature altar (#179)
    * added nature altar
    
    * added nature altar
    updated version

--- src/main/java/net/runelite/client/plugins/microbot/chillRunecraft/AutoRunecraftPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/chillRunecraft/AutoRunecraftPlugin.java
@@ -29,7 +29,7 @@ import java.awt.*;
 )
 @Slf4j
 public class AutoRunecraftPlugin extends Plugin {
-    public static final String version = "1.0.0";
+    public static final String version = "1.0.1";
     @Inject
     private AutoRunecraftConfig config;
     @Provides
