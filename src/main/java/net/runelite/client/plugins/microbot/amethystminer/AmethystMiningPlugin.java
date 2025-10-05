171afafc5bf7610c88cbcfd83a04689808bb8095
eqp48 <cdr48s@pm.me>
2025-10-05 14:28:26 -0500

    Fix: Reset mining script state on initialization to prevent text duplication in overlay (#205)
    * Fix: Reset mining script state on initialization to prevent text duplication in overlay
    
    * Update AmethystMiningPlugin.java
    
    Update version again as changes were made right before mine, remove Logger

--- src/main/java/net/runelite/client/plugins/microbot/amethystminer/AmethystMiningPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/amethystminer/AmethystMiningPlugin.java
@@ -28,7 +28,7 @@ import java.awt.*;
         isExternal = PluginConstants.IS_EXTERNAL
 )
 public class AmethystMiningPlugin extends Plugin {
-    public static final String version = "1.2.1";
+    public static final String version = "1.2.2";
     @Inject
     private OverlayManager overlayManager;
     @Inject
